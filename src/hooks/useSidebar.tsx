facbf1c669d18dccc61250bc9b72eaf2e8a813e2
prasojopxl <prasojodesigner@gmail.com>
2025-05-23 18:08:47 +0700

    format: prettier

--- src/hooks/useSidebar.tsx
+++ src/hooks/useSidebar.tsx
@@ -2,28 +2,28 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface iSidebarState {
-	sideBarShow: boolean;
-	setSideBarShow: (value: boolean) => void;
+    sideBarShow: boolean;
+    setSideBarShow: (value: boolean) => void;
 }
 
 interface iMainMenuState {
-	mainMenu: boolean;
-	setMainMenu: (value: boolean) => void;
+    mainMenu: boolean;
+    setMainMenu: (value: boolean) => void;
 }
 
-export const useSidebar = create<iSidebarState>((set) => ({
-	sideBarShow: true,
-	setSideBarShow: (value: boolean) => set({ sideBarShow: value }),
+export const useSidebar = create<iSidebarState>(set => ({
+    sideBarShow: true,
+    setSideBarShow: (value: boolean) => set({ sideBarShow: value }),
 }));
 
 export const useMainMenu = create<iMainMenuState>()(
-	persist(
-		(set) => ({
-			mainMenu: true,
-			setMainMenu: (value: boolean) => set({ mainMenu: value }),
-		}),
-		{
-			name: "mainmenu",
-		},
-	),
+    persist(
+        set => ({
+            mainMenu: true,
+            setMainMenu: (value: boolean) => set({ mainMenu: value }),
+        }),
+        {
+            name: "mainmenu",
+        },
+    ),
 );
